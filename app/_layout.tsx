a7a41b30c24bac3e9e57992b19199e0999373293
francistalbot <francistalbot@hotmail.com>
2025-11-12 16:25:27 -0500

    add protected route limited by isLoggedin

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,54 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-import { useEffect } from 'react';
-import { initDatabase } from '../database/initDatabase';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import { initDatabase } from "../database/initDatabase";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useColorScheme } from "@/hooks/use-color-scheme";
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: "(tabs)",
 };
 
+const isLoggedIn = false;
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   useEffect(() => {
     const setupDatabase = async () => {
-        try {
-          await initDatabase();
-          console.log('Base de données initialisée avec succès');
-        } catch (error) {
-          console.error('Erreur lors de l\'initialisation de la base de données:', error);
-        }
-      };
+      try {
+        await initDatabase();
+        console.log("Base de données initialisée avec succès");
+      } catch (error) {
+        console.error(
+          "Erreur lors de l'initialisation de la base de données:",
+          error
+        );
+      }
+    };
 
     setupDatabase();
   }, []);
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Protected guard={isLoggedIn}>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="modal"
+            options={{ presentation: "modal", title: "Modal" }}
+          />
+        </Stack.Protected>
+        <Stack.Protected guard={!isLoggedIn}>
+          <Stack.Screen name="signin" options={{ headerShown: false }} />
+          <Stack.Screen name="createAccount" options={{ headerShown: false }} />
+        </Stack.Protected>
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
