4be0a7dd9954bdb5751d074ef855c8dde5f5b9bc
francistalbot <francistalbot@hotmail.com>
2025-11-12 22:39:49 -0500

    Fix for Zustand middleware

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,26 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
 // Add wasm asset support
-config.resolver.assetExts.push('wasm');
- 
+config.resolver.assetExts.push("wasm");
+
+// Fix for Zustand middleware
+config.resolver.unstable_conditionNames = [
+  "browser",
+  "require",
+  "react-native",
+];
+
 // Add COEP and COOP headers to support SharedArrayBuffer
 config.server.enhanceMiddleware = (middleware) => {
   return (req, res, next) => {
-    res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
-    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    res.setHeader("Cross-Origin-Embedder-Policy", "credentialless");
+    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
     middleware(req, res, next);
   };
 };
- 
+
 module.exports = config;
