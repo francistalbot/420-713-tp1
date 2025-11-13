aa2d99d7d474352290cba14992dda62d192112f8
francistalbot <francistalbot@hotmail.com>
2025-11-12 22:40:25 -0500

    add zustand authStore

---
+++ utils/authStore.ts
@@ -0,0 +1,44 @@
+import * as SecureStore from "expo-secure-store";
+import { Platform } from "react-native";
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import { findUserByCredentials } from "../models/User";
+
+const isWeb = Platform.OS === "web";
+
+type UserState = {
+  userId: string | null;
+  logIn: (username: string, password: string) => Promise<void>;
+  logOut: () => Promise<void>;
+};
+
+export const useAuthStore = create(
+  persist<UserState>(
+    (set) => ({
+      userId: null,
+      logIn: async (username: string, password: string) => {
+        console.log("Tentative de connexion pour :", username);
+        const result = await findUserByCredentials(username, password);
+        console.log("Résultat de la connexion :", result);
+        if (result) {
+          set((state) => ({ ...state, userId: result.id }));
+        }
+      },
+
+      logOut: async () => {
+        set((state) => ({ ...state, userId: null }));
+      },
+    }),
+    {
+      name: "auth-store",
+      storage: isWeb
+        ? createJSONStorage(() => localStorage)
+        : createJSONStorage(() => ({
+            setItem: (key: string, value: string) =>
+              SecureStore.setItemAsync(key, value),
+            getItem: (key: string) => SecureStore.getItemAsync(key),
+            removeItem: (key: string) => SecureStore.deleteItemAsync(key),
+          })),
+    }
+  )
+);
