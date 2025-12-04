253e40bcd3689316741b05256823d7e23c0af5ed
Yamen Najeh <ennajehyamen@gmail.com>
2025-12-03 22:48:54 -0500

    migration vers firebase

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -1,7 +1,20 @@
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { initializeApp } from "firebase/app";
-import { connectAuthEmulator, getAuth } from "firebase/auth";
-import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
+import {
+  browserLocalPersistence,
+  connectAuthEmulator,
+  getReactNativePersistence,
+  initializeAuth,
+} from "firebase/auth";
+import {
+  connectFirestoreEmulator,
+  initializeFirestore,
+} from "firebase/firestore";
+import { Platform } from "react-native";
 
+// -----------------------------
+// CONFIG FIREBASE
+// -----------------------------
 const firebaseConfig = {
   apiKey: "AIzaSyBXg0pPRoVAx3AWMCmMLvkx-OCq9uSX3q4",
   authDomain: "android-e6481.firebaseapp.com",
@@ -11,14 +24,48 @@ const firebaseConfig = {
   appId: "1:618435124408:web:12b958fd32c01acd5b684d",
 };
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-/**/
+// Init App
+const app = initializeApp(firebaseConfig);
 
-if (__DEV__) {
-  connectFirestoreEmulator(db, "localhost", 8080);
-  connectAuthEmulator(auth, "http://localhost:9099");
-  console.log("Firestore connecté à l'émulateur local");
+// -----------------------------
+// AUTH (mobile vs web)
+// -----------------------------
+let auth;
+
+if (Platform.OS === "web") {
+  const { getAuth } = require("firebase/auth");
+  auth = getAuth(app);
+  auth.setPersistence(browserLocalPersistence);
+} else {
+  auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+  });
 }
+
+// -----------------------------
+// FIRESTORE : OBLIGATOIRE SUR ANDROID !!
+// -----------------------------
+export const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true,
+  useFetchStreams: false,
+});
+
+// -----------------------------
+// EMULATEURS (DEV uniquement)
+// -----------------------------
+if (__DEV__) {
+  const EMU_IP = "192.168.2.13"; // IP de ton PC !!!
+
+  try {
+    connectAuthEmulator(auth, `http://${EMU_IP}:9099`);
+    console.log("Auth connecté à l'émulateur local");
+
+    connectFirestoreEmulator(db, EMU_IP, 8080);
+    console.log("Firestore connecté à l'émulateur local");
+  } catch (e) {
+    console.log("Erreur connexion émulateur :", e);
+  }
+}
+
+export { auth };
+
